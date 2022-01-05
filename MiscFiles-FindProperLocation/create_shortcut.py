e52acb2bf2b92d200499adeff3593652c26ee494
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-01-05 10:32:11 -0500

    STY: code formatted with 'black' and fixed for 'flake8' compatiblitiy

--- MiscFiles-FindProperLocation/create_shortcut.py
+++ MiscFiles-FindProperLocation/create_shortcut.py
@@ -3,8 +3,8 @@ from pyshortcuts import make_shortcut
 
 
 root = os.path.dirname(os.path.join(__file__))
-app = os.path.join(root,'main.py')
-icon = os.path.join(root,'uis/pancake.ico')
+app = os.path.join(root, "main.py")
+icon = os.path.join(root, "uis/pancake.ico")
 
-make_shortcut(app, name='MIDAS', icon=icon)
+make_shortcut(app, name="MIDAS", icon=icon)
 print(f"MIDAS Desktop shortcut is created by { os.getlogin()}. ")
